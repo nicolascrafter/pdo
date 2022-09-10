b8b71c53b9e16424e0616d5603e526820e44ea6b
nicolascrafter <nicoer2611@gmail.com>
2022-09-10 17:35:36 -0300

    separado las funcione de ruteo y DB

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+    require_once "FunctionsDB.php";
+
+    function getUsuarios($dni = NULL) {
+        echo var_dump(getUsuariosDB($dni));
+    }
+
+    function postUsuarios() {
+        // var_dump($_POST);
+        if (isset($_POST["dni"]) && isset($_POST["nombre"])) {
+            if (!empty($_POST["dni"]) && !empty($_POST["nombre"])) {
+                $dni = intval($_POST["dni"]);
+                $nombre = $_POST["nombre"];
+                postUsuariosDB($dni, $nombre);
+                echo "formulario exitoso";
+            }
+        }
+        echo "El formulario no es valido";
+        // postUsuariosDB(44418075, "Mateo");
+    }
+
+    function showForm() {
+        require_once "template.php";
+    }
+    // echo var_dump(getUsuarios());
+    // echo "<br><br>";
+    // echo var_dump(getUsuarios(38106534));
--- router.php
+++ router.php
@@ -1,7 +1,7 @@
 <?php
 define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
-require_once "index.php";
+require_once "functions.php";
 
 //recibir/leer la accion
 if (!empty($_GET['action'])) {
