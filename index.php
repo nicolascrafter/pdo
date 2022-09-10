5e7537db154538f27ac2be9f0218a7da129c99ca
nicolascrafter <nicoer2611@gmail.com>
2022-09-10 16:47:17 -0300

    ruteo de usuarios

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
 <?php
-    function load_tareas() {
+    function load_tareas($id = NULL) {
         $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-        $sentencia = $db -> prepare("SELECT * FROM 'tareas'");
-        return $sentencia -> fetchAll(PDO::FETCH_OBJ);
+        if($id == NULL) {
+            $sentencia = $db -> prepare("SELECT * FROM tareas");
+            $sentencia -> execute();
+            return $sentencia -> fetchAll(PDO::FETCH_OBJ);
+        }
+        $sentencia = $db -> prepare("SELECT * FROM tareas WHERE id=?");
+        $sentencia -> execute([$id]);
+        return $sentencia -> fetch(PDO::FETCH_OBJ);
+        // var_dump($sentencia);
     }
 
-    echo var_dump(load_tareas());
-?>
+    function getUsuariosDB($dni = NULL) {
+        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+        if($dni == NULL) {
+            $sentencia = $db -> prepare("SELECT * FROM usuarios");
+            $sentencia -> execute();
+            return $sentencia -> fetchAll(PDO::FETCH_OBJ);
+        }
+        $sentencia = $db -> prepare("SELECT * FROM usuarios WHERE dni=?");
+        $sentencia -> execute([$dni]);
+        return $sentencia -> fetch(PDO::FETCH_OBJ);
+        // var_dump($sentencia);
+    }
+
+    function getUsuarios($dni = NULL) {
+        echo var_dump(getUsuariosDB($dni));
+    }
+
+    // echo var_dump(getUsuarios());
+    // echo "<br><br>";
+    // echo var_dump(getUsuarios(38106534));
---
+++ router.php
@@ -0,0 +1,28 @@
+<?php
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
+
+require_once "index.php";
+
+//recibir/leer la accion
+if (!empty($_GET['action'])) {
+    $accion = $_GET['action'];
+} else {
+    $accion = 'noticias';
+}
+
+// parseo el string de action por "/" y me devuelve el arreglo
+$params = explode('/', $accion);
+
+//
+switch ($params[0]) {
+    case 'usuarios':
+        if (empty($params[1])) {
+            getUsuarios();
+        } else {
+            getUsuarios($params[1]);
+        }
+        break;
+    default:
+        echo ('404 page not found');
+        break;
+}
